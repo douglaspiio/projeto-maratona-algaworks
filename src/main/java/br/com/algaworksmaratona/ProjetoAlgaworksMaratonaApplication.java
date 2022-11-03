d65d8ed2b3e83700aee83803e540cfbc6869609a
douglaspio <douglasp.alves@hotmail.com>
2022-11-03 19:40:35 -0300

    Importe do projeto para o repositório

---
+++ src/main/java/br/com/algaworksmaratona/ProjetoAlgaworksMaratonaApplication.java
@@ -0,0 +1,13 @@
+package br.com.algaworksmaratona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAlgaworksMaratonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAlgaworksMaratonaApplication.class, args);
+	}
+
+}
